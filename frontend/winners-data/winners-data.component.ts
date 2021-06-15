cd09fddddf176882d79ecd85c0fdf3eb0cc233a9
RawanKahlout <36929678+RawanKahlout@users.noreply.github.com>
2021-06-15 11:59:55 +0300

    latest update
    latest update

--- frontend/winners-data/winners-data.component.ts
+++ frontend/winners-data/winners-data.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { dataservice } from '../data.service';
 import {ActivatedRoute,Params ,Router} from '@angular/router'
 import {userServices} from '../user.service';
+import { element } from 'protractor';
+import { HttpClient  } from '@angular/common/http'; 
+
 @Component({
   selector: 'app-winners-data',
   templateUrl: './winners-data.component.html',
@@ -9,7 +12,7 @@ import {userServices} from '../user.service';
 })
 export class WinnersDataComponent implements OnInit {
 exist ;
-  constructor(private _dataservice: dataservice , private _route: ActivatedRoute , private _router : Router , private _userServices : userServices) {
+  constructor(private _dataservice: dataservice , private _route: ActivatedRoute , private _router : Router , private _userServices : userServices,private _Http :HttpClient) {
   this.getParams();
   this._dataservice.getCDetatils(this.routeParam).subscribe(
     data=>{
@@ -39,6 +42,7 @@ exist ;
   routeParam: Params
 
   ngOnInit() {
+    
     window.document.body.style.backgroundColor = "black";
 
     this._userServices.isActive().subscribe(
@@ -49,28 +53,25 @@ exist ;
   returnTable() {
     this.len = this.preTable.length;
     this.preTable.forEach(element => {
-      //this.dates.push(element.submitionData);
       this.dFeilds.push(element.formData)
     })
-    //this.tableHeader.push("submition Date");
+    this.i = 0;
+    this.tableHeader = Object.keys(this.dFeilds[this.dFeilds.length-1]);
     this.dFeilds.forEach(obj => {
-      Object.entries(obj).forEach(([key, value]) => {
-        if (this.i == 0) {
-          this.tableHeader.push(key)
-        }
-        this.dFeildsVal.push(value);
+      Object.keys(this.dFeilds[this.dFeilds.length-1]).forEach((element)=>{
+        this.dFeildsVal.push(obj[element]);
       })
-      this.i = 1;
     })
     this.matrix = Array.from({ length:this.len }, (_, i) => i)
       .map(i => this.dFeildsVal.slice(i * this.tableHeader.length, i * this.tableHeader.length + this.tableHeader.length));
-    //for (var i = 0 ; this.len ; i++)
-    //{this.matrix.push(this.dates[i])}
- 
-  }//-------------
+  }
   getParams(){
     this._route.params.subscribe(params => {
       return this.routeParam = params.id; 
     });
   }
+  download(){
+    this._dataservice.downloadFile(this.matrix,"Winners Data",this.tableHeader)
+  }
+
 }
