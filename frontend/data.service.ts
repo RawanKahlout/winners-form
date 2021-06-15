a4136ba63d438ba2fae47f52c299e6ae0ff4ab28
RawanKahlout <36929678+RawanKahlout@users.noreply.github.com>
2021-06-15 12:07:34 +0300

    latest update
    latest update

--- frontend/data.service.ts
+++ frontend/data.service.ts
@@ -16,4 +16,44 @@ export class dataservice {
     getCompetion() {
         return this._http.get<any[]>(apiUrl+'/api/allTemplates');
     }
+    downloadFile(data, filename='data',header) {
+            console.log(data,"hhh");
+        let csvData = this.ConvertToCSV(data,header);
+        console.log(csvData)
+        let blob = new Blob(['\ufeff' + csvData], { type: 'text/csv;charset=utf-8;' });
+        let dwldLink = document.createElement("a");
+        let url = URL.createObjectURL(blob);
+        let isSafariBrowser = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
+        if (isSafariBrowser) {  //if Safari open in new window to save file with random filename.
+            dwldLink.setAttribute("target", "_blank");
+        }
+        dwldLink.setAttribute("href", url);
+        dwldLink.setAttribute("download", filename + ".csv");
+        dwldLink.style.visibility = "hidden";
+        document.body.appendChild(dwldLink);
+        dwldLink.click();
+        document.body.removeChild(dwldLink);
+    }
+    ConvertToCSV(objArray, headerList) {
+        let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+        let str = '';
+        let row = 'S.No,';
+
+        for (let index in headerList) {
+            row += headerList[index] + ',';
+        }
+        row = row.slice(0, -1);
+        str += row + '\r\n';
+        for (let i = 0; i < array.length; i++) {
+            let line = (i+1)+'';
+            for (let index in headerList) {
+               let head = headerList[index];
+               var arrayElement =array[i];
+                line += ',' + arrayElement[index];
+
+            }
+            str += line + '\r\n';
+        }
+        return str;
+   }
 }
