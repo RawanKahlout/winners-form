138b45967a0f2911da52510ea56a281f3b2da1ad
RawanKahlout <36929678+RawanKahlout@users.noreply.github.com>
2021-06-15 11:30:10 +0300

    latest file
    latest file

--- backEnd/router/winnersData.js
+++ backEnd/router/winnersData.js
@@ -1,4 +1,5 @@
 const {winnerData} = require('../models/winner-data');
+const { formTemplate } = require('../models/form-template');
 module.exports = (app) => {
     //submitwinnerData 
     app.post('/api/submitWinnerData', (req, res) => {
@@ -7,14 +8,48 @@ module.exports = (app) => {
         //var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
         //var yyyy = today.getFullYear();
         //today = Date.now();
-        new winnerData({
-            submitionData: Date.now(),
-            formData: req.body.formData,
-            shortId: req.body.shortId,
-            competitionName: req.body.competitionName
-        }).save().then((result) => {
-            return res.json(result).status(200);
-        })
+        let shortId = req.body.shortId
+        let uniqueValues;
+        let ip = req.headers['x-forwarded-for'] ||
+            req.connection.remoteAddress ||
+            req.socket.remoteAddress ||
+            (req.connection.socket ? req.connection.socket.remoteAddress : null);
+        let response;
+        let isUnique = false;
+        formTemplate.findOne({ shortId: shortId }).then((qResult) => {
+            winnerData.findOne({ Ip: ip, shortId: req.body.shortId }).then((winnerObj) => {
+                if (winnerObj) { response = "معلوماتك بالفعل موجودة"; return res.status(400).send(response); }
+                qResult.extraField.forEach((elem) => {
+                    if (elem.type == "unique") {
+                        isUnique = true;
+                        uniqueValues = elem.val;
+                        if (uniqueValues.includes(req.body.formData[elem.name])) {
+                            new winnerData({
+                                submitionData: Date.now(),
+                                formData: req.body.formData,
+                                shortId: req.body.shortId,
+                                competitionName: req.body.competitionName,
+                                Ip: ip
+                            }).save().then((result) => {
+                                return res.json(result).status(200);
+                            })
+                        }
+                        else { response = "عذراُ غير مصرح لك باستخدام النموذج"; return res.status(404).send(response); }
+                    }
+                });
+                if (!isUnique) {
+                    new winnerData({
+                        submitionData: Date.now(),
+                        formData: req.body.formData,
+                        shortId: req.body.shortId,
+                        competitionName: req.body.competitionName,
+                        Ip: ip
+                    }).save().then((result) => {
+                        return res.json(result).status(200);
+                    })
+                }
+            })
+        });
     })
     //get all winnerData
     app.get('/api/GetWinnersData', async(req , res)=>{
